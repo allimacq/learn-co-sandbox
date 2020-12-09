ce34f5863d5883c2b89babac687ae325eb50d73d
Allison _ <allimacq@gmail.com>
2020-12-09 17:01:15 +0000

    changed directory name

---
+++ accessibility/lib/trail_accessibility/CLI.rb
@@ -0,0 +1,10 @@
+ #using namespacing so we avoid overwriting anything.
+class TrailAccessibility::CLI
+  
+  def call
+    puts "Welcome!"
+    puts "Which State would you like to explore for accessible trails?"
+    puts "(Distric of Columbia is also an option!"
+  end
+  
+end
